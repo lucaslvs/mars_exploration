51991669bd33162a13bdde70d4d7559073f01e31
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-05-15 02:48:07 -0300

    add test for Probe.perform_action/2

--- test/mars_exploration/probe_test.exs
+++ test/mars_exploration/probe_test.exs
@@ -43,57 +43,141 @@ defmodule MarsExploration.ProbeTest do
     test "must turn direction to W(est) when receive L(eft) action and current direction is N(orth)" do
       params = %{direction: "N", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "L")
       assert updated_probe.direction == "W"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
     end
 
     test "must turn direction to E(ast) when receive R(ight) action and current direction is N(orth)" do
       params = %{direction: "N", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "R")
       assert updated_probe.direction == "E"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
+    end
+
+    test "must increment column value when receive M(ove) action and current direction is N(orth)" do
+      params = %{direction: "N", column: 0, line: 0}
+      probe = Probe.new(params)
+
+      assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "M")
+      assert updated_probe.direction == "N"
+      assert updated_probe.column == 1
+      assert updated_probe.line == 0
     end
 
     test "must turn direction to S(outh) when receive L(eft) action and current direction is W(est)" do
       params = %{direction: "W", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "L")
       assert updated_probe.direction == "S"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
     end
 
     test "must turn direction to N(orth) when receive R(ight) action and current direction is W(est)" do
       params = %{direction: "W", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "R")
       assert updated_probe.direction == "N"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
+    end
+
+    test "must decrement line value when receive M(ove) action and current direction is W(est)" do
+      params = %{direction: "W", column: 0, line: 1}
+      probe = Probe.new(params)
+
+      assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "M")
+      assert updated_probe.direction == "W"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
+    end
+
+    test "must not decrement line value when receive M(ove) action and current direction is W(est)" do
+      params = %{direction: "W", column: 0, line: 0}
+      probe = Probe.new(params)
+
+      assert {:error, :invalid_action, probe} = Probe.perform_action(probe, "M")
+      assert probe.direction == "W"
+      assert probe.column == 0
+      assert probe.line == 0
     end
 
     test "must turn direction to E(ast) when receive L(eft) action and current direction is S(outh)" do
       params = %{direction: "S", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "L")
       assert updated_probe.direction == "E"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
     end
 
     test "must turn direction to W(est) when receive R(ight) action and current direction is S(outh)" do
       params = %{direction: "S", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "R")
       assert updated_probe.direction == "W"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
+    end
+
+    test "must decrement column value when receive M(ove) action and current direction is S(outh)" do
+      params = %{direction: "S", column: 1, line: 0}
+      probe = Probe.new(params)
+
+      assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "M")
+      assert updated_probe.direction == "S"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
+    end
+
+    test "must not decrement column value when receive M(ove) action and current direction is S(outh)" do
+      params = %{direction: "S", column: 0, line: 0}
+      probe = Probe.new(params)
+
+      assert {:error, :invalid_action, probe} = Probe.perform_action(probe, "M")
+      assert probe.direction == "S"
+      assert probe.column == 0
+      assert probe.line == 0
     end
 
     test "must turn direction to N(orth) when receive L(eft) action and current direction is E(ast)" do
       params = %{direction: "E", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "L")
       assert updated_probe.direction == "N"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
     end
 
     test "must turn direction to N(orth) when receive R(ight) action and current direction is E(ast)" do
       params = %{direction: "E", column: 0, line: 0}
       probe = Probe.new(params)
+
       assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "R")
       assert updated_probe.direction == "S"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 0
+    end
+
+    test "must increment line value when receive M(ove) action and current direction is E(ast)" do
+      params = %{direction: "E", column: 0, line: 0}
+      probe = Probe.new(params)
+
+      assert {:ok, probe, updated_probe} = Probe.perform_action(probe, "M")
+      assert updated_probe.direction == "E"
+      assert updated_probe.column == 0
+      assert updated_probe.line == 1
     end
   end
 end
