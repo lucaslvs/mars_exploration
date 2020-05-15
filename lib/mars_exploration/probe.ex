bf7d9720c40a359dd582011e6a498626ba110bf8
Lucas Varlesse <lucas.varlesse@redealumni.com>
2020-05-14 23:27:15 -0300

    add Probe.perform_action/2 function

--- lib/mars_exploration/probe.ex
+++ lib/mars_exploration/probe.ex
@@ -13,6 +13,70 @@ defmodule MarsExploration.Probe do
     end
   end
 
+  def perform_action(%__MODULE__{} = probe, action) do
+    cond do
+      not action_is_valid?(action) ->
+        {:error, :invalid_move, probe}
+
+      action == "L" ->
+        {:ok, probe, turn_left(probe)}
+
+      action == "R" ->
+        {:ok, probe, turn_right(probe)}
+
+      true ->
+        {:ok, probe, move(probe)}
+    end
+  end
+
+  defp move(%__MODULE__{direction: direction, column: column, line: line} = probe) do
+    case direction do
+      "N" ->
+        Map.put(probe, :column, column + 1)
+
+      "S" ->
+        Map.put(probe, :column, column - 1)
+
+      "E" ->
+        Map.put(probe, :line, line + 1)
+
+      "W" ->
+        Map.put(probe, :line, line - 1)
+    end
+  end
+
+  defp turn_left(%__MODULE__{direction: direction} = probe) do
+    case direction do
+      "N" ->
+        Map.put(probe, :direction, "W")
+
+      "S" ->
+        Map.put(probe, :direction, "E")
+
+      "E" ->
+        Map.put(probe, :direction, "N")
+
+      "W" ->
+        Map.put(probe, :direction, "S")
+    end
+  end
+
+  defp turn_right(%__MODULE__{direction: direction} = probe) do
+    case direction do
+      "N" ->
+        Map.put(probe, :direction, "E")
+
+      "S" ->
+        Map.put(probe, :direction, "W")
+
+      "E" ->
+        Map.put(probe, :direction, "S")
+
+      "W" ->
+        Map.put(probe, :direction, "N")
+    end
+  end
+
   defp params_are_valid?(params) when is_map(params) do
     direction_is_valid?(params) and position_is_valid?(params)
   end
@@ -29,4 +93,8 @@ defmodule MarsExploration.Probe do
       Map.get(params, :column) >= 0 and
       Map.get(params, :line) >= 0
   end
+
+  defp action_is_valid?(action) when is_binary(action) and action in @valid_actions, do: true
+
+  defp action_is_valid?(_action), do: false
 end
--- test/mars_exploration_test.exs
+++ test/mars_exploration_test.exs
@@ -1,41 +1,7 @@
-defmodule MarsExploration.ProbeTest do
+defmodule MarsExplorationTest do
   use ExUnit.Case
 
-  alias MarsExploration.Probe
-
-  describe "new/1" do
-    @valid_params [
-      %{direction: "N", column: 0, line: 0},
-      %{direction: "S", column: :rand.uniform(100), line: :rand.uniform(100)},
-      %{direction: "E", column: :rand.uniform(100), line: :rand.uniform(100)},
-      %{direction: "W", column: :rand.uniform(100), line: :rand.uniform(100)}
-    ]
-
-    @invalid_params [
-      Map.new(),
-      %{direction: nil, column: nil, line: nil},
-      %{direction: nil, column: nil},
-      %{direction: nil},
-      %{direction: :rand.uniform(100), column: "nil", line: "2"},
-      %{direction: "N", column: [], line: -1},
-      %{direction: "S", column: -12, line: :rand.uniform(100)}
-    ]
-
-    test "must return a probe when receiving valid parameters" do
-      Enum.each(@valid_params, fn params ->
-        probe = Probe.new(params)
-
-        assert %Probe{} = probe
-        assert probe.direction == params.direction
-        assert probe.column == params.column
-        assert probe.line == params.line
-      end)
-    end
-
-    test "must raise a ArgumentError when receiving invalid parameters" do
-      Enum.each(@invalid_params, fn params ->
-        assert_raise ArgumentError, "Invalid params", fn -> Probe.new(params) end
-      end)
-    end
+  test "greets the world" do
+    assert MarsExploration.hello() == :world
   end
 end
